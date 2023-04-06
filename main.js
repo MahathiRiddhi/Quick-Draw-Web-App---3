a06d8adb3d25353a21de70cc6250355153f2eae3
MahathiRiddhi <99087413+MahathiRiddhi@users.noreply.github.com>
2023-04-06 16:16:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+array_1 = ["aircraft carrier", "airplane", "alarm clock", "ambulance", "angel", "animal migration", "ant", "anvil", "apple", "arm", "asparagus", "axe", "backpack", "banana", "bandage", "barn", "baseball", "baseball bat", "basket", "basketball", "bat", "bathtub", "beach", "bear", "beard", "bed", "bee", "belt", "bench", "bicycle", "binoculars", "bird", "birthday cake", "blackberry", "blueberry", "book", "boomerang", "bottlecap", "bowtie", "bracelet", "brain", "bread", "bridge", "broccoli", "broom", "bucket", "bulldozer", "bus", "bush", "butterfly", "cactus", "cake", "calculator", "calendar", "camel", "camera", "camouflage", "campfire", "candle", "cannon", "canoe", "car", "carrot", "castle", "cat", "ceiling fan", "cello", "cell phone", "chair", "chandelier", "church", "circle", "clarinet", "clock", "cloud", "coffee cup", "compass", "computer", "cookie", "cooler", "couch", "cow", "crab", "crayon", "crocodile", "crown", "cruise ship", "cup", "diamond", "dishwasher", "diving board", "dog", "dolphin", "donut", "door", "dragon", "dresser", "drill", "drums", "duck", "dumbbell", "ear", "elbow", "elephant", "envelope", "eraser", "eye", "eyeglasses", "face", "fan", "feather", "fence", "finger", "fire hydrant", "fireplace", "firetruck", "fish", "flamingo", "flashlight", "flip flops", "floor lamp", "flower", "flying saucer", "foot", "fork", "frog", "frying pan", "garden", "garden hose", "giraffe", "goatee", "golf club", "grapes", "grass", "guitar", "hamburger", "hammer", "hand", "harp", "hat", "headphones", "hedgehog", "helicopter", "helmet", "hexagon", "hockey puck", "hockey stick", "horse", "hospital", "hot air balloon", "hot dog", "hot tub", "hourglass", "house", "house plant", "hurricane", "ice cream", "jacket", "jail", "kangaroo", "key", "keyboard", "knee", "knife", "ladder", "lantern", "laptop", "leaf", "leg", "light bulb", "lighter", "lighthouse", "lightning", "line", "lion", "lipstick", "lobster", "lollipop", "mailbox", "map", "marker", "matches", "megaphone", "mermaid", "microphone", "microwave", "monkey", "moon", "mosquito", "motorbike", "mountain", "mouse", "moustache", "mouth", "mug", "mushroom", "nail", "necklace", "nose", "ocean", "octagon", "octopus", "onion", "oven", "owl", "paintbrush", "paint can", "palm tree", "panda", "pants", "paper clip", "parachute", "parrot", "passport", "peanut", "pear", "peas", "pencil", "penguin", "piano", "pickup truck", "picture frame", "pig", "pillow", "pineapple", "pizza", "pliers", "police car", "pond", "pool", "popsicle", "postcard", "potato", "power outlet", "purse", "rabbit", "raccoon", "radio", "rain", "rainbow", "rake", "remote control", "rhinoceros", "rifle", "river", "roller coaster", "rollerskates", "sailboat", "sandwich", "saw", "saxophone", "school bus", "scissors", "scorpion", "screwdriver", "sea turtle", "see saw", "shark", "sheep", "shoe", "shorts", "shovel", "sink", "skateboard", "skull", "skyscraper", "sleeping bag", "smiley face", "snail", "snake", "snorkel", "snowflake", "snowman", "soccer ball", "sock", "speedboat", "spider", "spoon", "spreadsheet", "square", "squiggle", "squirrel", "stairs", "star", "steak", "stereo", "stethoscope", "stitches", "stop sign", "stove", "strawberry", "streetlight", "string bean", "submarine", "suitcase", "sun", "swan", "sweater", "swingset", "sword", "syringe", "table", "teapot", "teddy-bear", "telephone", "television", "tennis racquet", "tent", "The Eiffel Tower", "The Great Wall of China", "The Mona Lisa", "tiger", "toaster", "toe", "toilet", "tooth", "toothbrush", "toothpaste", "tornado", "tractor", "traffic light", "train", "tree", "triangle", "trombone", "truck", "trumpet", "tshirt", "umbrella", "underwear", "van", "vase", "violin", "washing machine", "watermelon", "waterslide", "whale", "wheel", "windmill", "wine bottle", "wine glass", "wristwatch", "yoga", "zebra", "zigzag"]
+var random_no = Math.floor((Math.random() * array_1.length) + 1)
+console.log(random_no)
+sketch = array_1[random_no]
+timeCounter = 0
+timeCheck = ""
+drawnSketch = ""
+answer = ""
+score = ""
+document.getElementById("sketch").innerHTML = sketch
+function preload() {
+    classifier = ml5.imageClassifier("DoodleNet")
+}
+function setup() {
+    canvas = createCanvas(400, 400)
+    canvas.center()
+    classifyCanvas()
+}
+function draw() {
+
+    if (mouseIsPressed) {
+        strokeWeight(30)
+        stroke("plum")
+        line(pmouseX, pmouseY, mouseX, mouseY)
+    }
+    checkSketch()
+
+    if (drawnSketch = sketch) {
+        answer = "set"
+         score++
+        document.getElementById("score").innerHTML = score
+    }
+}
+function checkSketch() {
+    timeCounter++
+    document.getElementById("timer").innerHTML = timeCounter
+    console.log(timeCounter)
+    if (timeCounter > 400) {
+        timeCounter = 0
+        timeCheck = "completed"
+    }
+    if (timeCheck == "completed" || answer == "set") {
+        timeCheck = ""
+        answer = ""
+        updateSketch()
+    }
+
+}
+function updateSketch() {
+
+}
+function classifyCanvas() {
+    classifier.classify(canvas, gotResults)
+}
+function gotResults(error, results) {
+    if (error) {
+        console.log(error)
+    }
+    else {
+        console.log(results)
+        drawnSketch = results[0].label
+        document.getElementById("sketchName").innerHTML = drawnSketch
+        confidence = results[0].confidence
+        document.getElementById("confidence").innerHTML = Math.round(results[0].confidence * 100) + "%"
+
+    }
+}
